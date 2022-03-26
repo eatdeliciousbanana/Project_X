f18e895fbb3d3adf960bc8358ba14e981ffe35f5
eatdeliciousbanana <93957952+eatdeliciousbanana@users.noreply.github.com>
2022-03-27 03:02:23 +0900

    ゲーム画面遷移を実装

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,122 +1,93 @@
+let typingWords = [];  // 文字
+
 function typingGame() {
-    let words = [];    // 文字
-    let index = 0;     // 文字のインデックス
-    let untyped = '';  // 打ってない文字
-    let typed = '';    // 打った文字
-    let score = 0;     // 得点
+    // ゲーム読み込み
+    switchScreen('loading');
+    loadClock();
+    loadKeyboard();
+    loadTitle();
+    loadConfig();
+    loadMode();
+    loadResult();
+    getWords();  // jsonから文字取得
 
-    // jsonから文字取得
-    getWords(words);
-
-    // 文字フィールドを初期化
+    // ゲーム読み込み完了
     window.setTimeout(function () {
-        index = getRandom(0, words.length);
-        untyped = words[index]['rome'];
-        updateField(words[index], score);
+        switchScreen('title');
     }, 1000);
+}
 
-    let lightKey = '';  // 点灯中のキー
 
-    // カウントダウン処理
-    let count = 20;
-    countDown = () => {
-        const timeoutId = setTimeout(countDown, 1000);
-        $('#time').html('残り' + count-- + '秒');
-        if (count === -1) {
-            clearTimeout(timeoutId);  //timeoutIdをclearTimeoutで指定している
-            $('#result-img').show(); //結果を表示する
+/* 各ゲーム画面を遷移させる関数
+   表示させたい画面の名前: screenName
+   ロード画面: loading
+   タイトル画面: title
+   設定画面: config
+   モード選択画面: mode
+   スペースキーを押して開始: space
+   プレイ画面: playing
+   リザルト画面: result
+*/
+function switchScreen(screenName) {
+    $('.switch_screen').each(function () {
+        $(this).hide();
+    });
+    $(`#gamescreen_${screenName}`).show();
+
+    if (screenName === 'space') {
+        document.addEventListener('keydown', switchScreenAtSpace);
+    }
+
+    function switchScreenAtSpace(event) {
+        if (event.code === 'Space') {
+            loadPlaying();
+            switchScreen('playing');
+            this.removeEventListener('keydown', switchScreenAtSpace);
         }
     }
-    // countDown();
+}
 
-    // キー入力したときの処理
 
+// 時計が動くようにする関数
+function loadClock() {
+    setInterval(() => {
+        // 現在時間の取得
+        const now = new Date();
+        const h = now.getHours();
+        const m = now.getMinutes();
+        const s = now.getSeconds();
+
+        // 時、分、秒を元に角度を計算
+        const degH = h * (360 / 12) + m * (360 / 12 / 60);
+        const degM = m * (360 / 60);
+        const degS = s * (360 / 60);
+
+        // 各要素を取得
+        const elementH = document.querySelector(".c-clock__hour");
+        const elementM = document.querySelector(".c-clock__min");
+        const elementS = document.querySelector(".c-clock__sec");
+
+        // styleを追加
+        elementH.style.transform = `rotate(${degH}deg)`;
+        elementM.style.transform = `rotate(${degM}deg)`;
+        elementS.style.transform = `rotate(${degS}deg)`;
+    }, 10);
+}
+
+
+// キーボードが動くようにする関数
+function loadKeyboard() {
+    let lightKey = '';  // 点灯中のキー
     document.addEventListener('keydown', function (event) {
-
-        // キー点灯
         $('#key_' + lightKey).css('background-color', 'rgb(243, 243, 243)');
         lightKey = event.code;
         $('#key_' + lightKey).css('background-color', 'orange');
-
-        // 入力したキーが、打ってない文字の先頭であった場合
-        if (event.key === untyped.charAt(0)) {
-            // 打ってない文字の先頭を打った文字の末尾に追加
-            typed += untyped.charAt(0);
-            untyped = untyped.slice(1);
-
-            // 全部打ち終わったら新しい文字にする
-            if (untyped === '') {
-                index = getRandom(0, words.length);
-                untyped = words[index]['rome'];
-                typed = '';
-                score += 100;
-                updateField(words[index], score);
-                return;
-            }
-
-            // HTML要素に反映
-            $('#untyped').html(untyped);
-            $('#typed').html(typed);
-        }
     });
-
 }
-
-var val;
-// 一定間隔で処理を行うintervalのIDを保持
-var intervalID;
-
-// ボタンを押した時に動く関数
-function begin() {
-    val = 0;
-    // ボタンを無効にする(何回も押せないように)
-    document.getElementById("begin").disabled = true;
-    // 1000msおきにプログレスバーを更新する
-    intervalID = setInterval("updateProgress()", 1000);
-}
-
-// プログレスバーを更新する
-function updateProgress() {
-    // プログレスバーの進捗値を更新し、プログレスバーに反映させる
-    val += 1;
-    document.getElementById("myProgress").value = val;
-    document.getElementById("myProgress").innerText = val + "%";
-
-    // 最大値まで達したら終了
-    if (val == 20) {
-        clearInterval(intervalID);
-        document.getElementById("begin").disabled = false;
-    }
-}
-
-
-// 時計
-setInterval(() => {
-    // 現在時間の取得
-    const now = new Date();
-    const h = now.getHours();
-    const m = now.getMinutes();
-    const s = now.getSeconds();
-
-    // 時、分、秒を元に角度を計算
-    const degH = h * (360 / 12) + m * (360 / 12 / 60);
-    const degM = m * (360 / 60);
-    const degS = s * (360 / 60);
-
-    // 各要素を取得
-    const elementH = document.querySelector(".c-clock__hour");
-    const elementM = document.querySelector(".c-clock__min");
-    const elementS = document.querySelector(".c-clock__sec");
-
-    // styleを追加
-    elementH.style.transform = `rotate(${degH}deg)`;
-    elementM.style.transform = `rotate(${degM}deg)`;
-    elementS.style.transform = `rotate(${degS}deg)`;
-}, 10);
 
 
 // jsonから文字のオブジェクトを取ってくる関数
-function getWords(words) {
+function getWords() {
     $.ajax({
         type: 'GET',
         url: './words.json',
@@ -128,7 +99,7 @@ function getWords(words) {
                 let data_stringify = JSON.stringify(json);
                 let data_json = JSON.parse(data_stringify);
                 data_json.forEach(function (word) {
-                    words.push(word);
+                    typingWords.push(word);
                 });
             },
             // エラー発生時
@@ -139,17 +110,117 @@ function getWords(words) {
 }
 
 
-// 文字フィールドに新たな文字を設定する関数
-function updateField(word, s) {
-    $('#kanzi_field').html(word['kanzi']);
-    $('#kana_field').html('（' + word['kana'] + '）');
-    $('#untyped').html(word['rome']);
-    $('#typed').html('');
-    $('#score').html('現在' + s + '点');
+// タイトル画面
+function loadTitle() {
+
 }
 
 
-// m以上n未満のランダムな整数を返す関数
-function getRandom(m, n) {
-    return Math.floor(Math.random() * (n - m)) + m;
+// 設定画面
+function loadConfig() {
+
+}
+
+
+// モード画面
+function loadMode() {
+
+}
+
+
+// プレイ画面
+function loadPlaying() {
+    let index = 0;     // 文字のインデックス
+    let untyped = '';  // 打ってない文字
+    let typed = '';    // 打った文字
+    let score = 0;     // 得点
+
+    // 文字フィールドを初期化
+    index = getRandom(0, typingWords.length);
+    untyped = typingWords[index]['rome'];
+    updateField(typingWords[index], score);
+
+    // キー入力したときの処理
+    document.addEventListener('keydown', typeKey);
+
+
+    // キー入力したときの処理の関数
+    function typeKey(event) {
+        // 入力したキーが、打ってない文字の先頭であった場合
+        if (event.key === untyped.charAt(0)) {
+            // 打ってない文字の先頭を打った文字の末尾に追加
+            typed += untyped.charAt(0);
+            untyped = untyped.slice(1);
+
+            // 全部打ち終わったら新しい文字にする
+            if (untyped === '') {
+                index = getRandom(0, typingWords.length);
+                untyped = typingWords[index]['rome'];
+                typed = '';
+                score += 100;
+                updateField(typingWords[index], score);
+                return;
+            }
+
+            // HTML要素に反映
+            $('#untyped').html(untyped);
+            $('#typed').html(typed);
+        }
+    }
+
+
+    // 文字フィールドに新たな文字を設定する関数
+    function updateField(word, s) {
+        $('#kanzi_field').html(word['kanzi']);
+        $('#kana_field').html(word['kana']);
+        $('#untyped').html(word['rome']);
+        $('#typed').html('');
+        $('#score').html('現在' + s + '点');
+    }
+
+
+    // m以上n未満のランダムな整数を返す関数
+    function getRandom(m, n) {
+        return Math.floor(Math.random() * (n - m)) + m;
+    }
+
+
+    // カウントダウン処理
+    let count = 20;
+    function countDown() {
+        const timeoutId = setTimeout(countDown, 1000);
+        $('#time').html('残り' + count-- + '秒');
+        if (count === -1) {
+            clearTimeout(timeoutId);  //timeoutIdをclearTimeoutで指定している
+            document.removeEventListener('keydown', typeKey);
+            switchScreen('result');  //結果を表示する
+        }
+    }
+    countDown();
+
+
+    // 1000msおきにプログレスバーを更新する
+    let val = 0;
+    let intervalID;
+    intervalID = setInterval(updateProgress, 1000);
+
+    // プログレスバーを更新する
+    function updateProgress() {
+        // プログレスバーの進捗値を更新し、プログレスバーに反映させる
+        val += 1;
+        document.getElementById("myProgress").value = val;
+        document.getElementById("myProgress").innerText = val + "%";
+
+        // 最大値まで達したら終了
+        if (val == 20) {
+            clearInterval(intervalID);
+            document.getElementById("begin").disabled = false;
+        }
+    }
+}
+
+
+// リザルト画面
+function loadResult() {
+
 }
