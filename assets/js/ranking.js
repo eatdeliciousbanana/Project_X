87997915dcd0d86fca76c387831747c1cec328be
ogakihiromu <umashiogiro1001@gmail.com>
2022-07-18 00:43:26 +0900

    ・ランキング画面の作成
    -変更したファイル:ranking.htmlの削除、ranking_elem.html,ranking_high.html,ranking_mid.htmlの追加、ranking.jsの加筆、ranking.cssの加筆
    
    -変更点:小学生,中学生,高校生別のランキングページの作成、データベースからランキングページに5位までの名前を表示

--- assets/js/ranking.js
+++ assets/js/ranking.js
@@ -1,16 +1,55 @@
+// $ = jQuery,$はjQueryのライブラリを示す
 $(function () {
     let ranking = {};  // ランキング
     getRanking();
+
+    // ランキングの読込みに時間がかかるために1秒後にランキングの要素にアクセス
     setTimeout(function () {
         console.log(ranking);
+        writeHtml("elem_math", 5);
+        writeHtml("elem_jpn", 5);
+        writeHtml("elem_eng", 5);
+        writeHtml("elem_sci", 5);
+        writeHtml("elem_social", 5);
+
+        writeHtml("mid_math", 5);
+        writeHtml("mid_jpn", 5);
+        writeHtml("mid_eng", 5);
+        writeHtml("mid_sci", 5);
+        writeHtml("mid_social", 5);
+
+        writeHtml("high_math", 5);
+        writeHtml("high_jpn", 5);
+        writeHtml("high_eng", 5);
+        writeHtml("high_chem", 5);
+        writeHtml("high_physics", 5);
+        writeHtml("high_geography", 5);
+        writeHtml("high_bio", 5);
+        writeHtml("high_worldhis", 5);
+        writeHtml("high_japanhis", 5);
+        writeHtml("high_geology", 5);
     }, 1000);
 
-    // 全教科のランキングを取得する関数
+    function writeHtml(subj, range) {
+
+        for(let i = 0;i < range; i++){
+            if(typeof ranking[subj][i] === "undefined"){
+                console.log("worning: Access violation in ranking '" + subj + "' array.")
+                break;
+            }
+            $("ol." + subj + " > li").eq(i).html(ranking[subj][i]["name"]);  // 左から1番目の()は書き込みたいhtmlの要素、2番目のeq()でhtmlにおける要素の順番指定、3番目のhtml()でhtmlに書き込む文字列を指定
+        }
+
+    }
+
+    // 全教科のランキングを取得する関数[]
     function getRanking() {
+
+        // ランキングをデータベースから取得するphpを呼び出す
         $.ajax({
             type: 'GET',
-            url: '/backend/get_ranking.php'
-        }).then(
+            url: 'backend/get_ranking.php'
+        }).then(        // ajax通信のリクエストの成功、失敗に実行される関数
             // 成功時
             function (data, textStatus, jqXHR) {
                 if (typeof data !== 'object') {
